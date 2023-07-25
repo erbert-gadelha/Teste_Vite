3d47c25827eb5c0b466213e645c05f5bfc96f822
ebgr <ebgr@g3c35.cin.ufpe.br>
2023-07-25 14:25:35 -0300

    Tela refatorada

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,11 @@ import Provider from "./Provider";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider>
-      <App />
+      <div>
+      	<h1>Tela Inicial</h1>
+      	<input type="text" placeholder="pesquisar"></input>
+      	<button>pesquisar</button>
+      </div>
     </Provider>
   </React.StrictMode>
 );
