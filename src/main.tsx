e4a230922506858ccf91de900ae38f1d96464f23
ebgr <ebgr@g3c35.cin.ufpe.br>
2023-07-25 14:38:50 -0300

    Refatorando 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+//import App from "./App";
 import "./global.css";
-import Provider from "./Provider";
+//import Provider from "./Provider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider>
+  //<React.StrictMode>
+  //  <Provider>
       <div>
       	<h1>Tela Inicial</h1>
       	<input type="text" placeholder="pesquisar"></input>
       	<button>pesquisar</button>
       </div>
-    </Provider>
-  </React.StrictMode>
+  //  </Provider>
+  //</React.StrictMode>
 );
